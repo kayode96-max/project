f3f8035283166c0b113ff731bd2f48821957d961
Kayode96max <olukayodeusman406@gmail.com>
2025-06-16 14:33:00 +0100

    click counter app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  let increaseButton = {id: "increase", name: "+"}
+  let decreaseButton = {id: "decrease", name: "-"}
+
+  function handleIncrease() {
+    if (count < 1000) {
+      setCount(count + 1);
+    } else {
+      alert("You can't exceed the limit");
+    }
+  }
+
+  function handleDecrease() {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      alert("You can't go below zero");
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+        <h1>Click counter</h1>
       </header>
+      <div className="display">
+        <h3 className="count" id='counter' key={count}>{count}</h3>
+      </div>
+      <div className="buttons">
+        <button id={increaseButton.id} onClick={handleIncrease}> {increaseButton.name} </button>
+        <button id={decreaseButton.id} onClick={handleDecrease}> {decreaseButton.name} </button>
+      </div>
     </div>
   );
 }
